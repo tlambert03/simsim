f78c924d85f6a30d37f7a1cac79a621635af1a5f
Talley Lambert <talley.lambert@gmail.com>
2019-08-27 23:34:42 -0400

    happy with 2d 3d transforms

--- simsim/cuda/transform.cu
+++ simsim/cuda/transform.cu
@@ -1,10 +1,62 @@
-//#include <stdio.h>
+#include <stdio.h>
 
-texture<float, cudaTextureType3D, cudaReadModeElementType> texRef;
+texture<float, cudaTextureType2D, cudaReadModeElementType> texref2d;
 
-// Simple transformation kernel
-__global__ void transformKernel(float *output, int nx, int ny, int nz,
-                                float *mat) {
+texture<float, cudaTextureType3D, cudaReadModeElementType> texref3d;
+
+__global__ void affine2D(float *output, int nx, int ny, float *mat) {
+
+  // Calculate texture coordinates
+  unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (x >= nx || y >= ny) {
+    return;
+  }
+
+  float u = x;
+  float v = y;
+
+  float tu = mat[0] * u + mat[1] * v + mat[2] + 0.5f;
+  float tv = mat[3] * u + mat[4] * v + mat[5] + 0.5f;
+
+  // Read from texture and write to global memory
+  int idx = y * nx + x;
+  output[idx] = tex2D(texref2d, tu, tv);
+}
+
+__global__ void affine2D_RA(float *output, int nx, int ny, float dx, float dy,
+                            float *mat) {
+
+  // Calculate texture coordinates
+  unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+  unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (x >= nx || y >= ny) {
+    return;
+  }
+
+  float u = x;
+  float v = y;
+
+  // intrinsic coords to world
+  u = 0.5 + (u - 0.5) * dx;
+  v = 0.5 + (v - 0.5) * dy;
+
+  // transform coordinates in world coordinate frame
+  float tu = mat[0] * u + mat[1] * v + mat[2];
+  float tv = mat[3] * u + mat[4] * v + mat[5];
+
+  // world coords to intrinsic
+  tu = 0.5 + (tu - 0.5) / dx;
+  tv = 0.5 + (tv - 0.5) / dy;
+
+  // Read from texture and write to global memory
+  int idx = y * nx + x;
+  output[idx] = tex2D(texref2d, tu, tv);
+}
+
+__global__ void affine3D(float *output, int nx, int ny, int nz, float *mat) {
 
   // Calculate texture coordinates
   unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -25,17 +77,15 @@ __global__ void transformKernel(float *output, int nx, int ny, int nz,
 
   // Read from texture and write to global memory
   int idx = z * (nx * ny) + y * nx + x;
-  output[idx] = tex3D(texRef, tu, tv, tw);
+  output[idx] = tex3D(texref3d, tu, tv, tw);
 
-  // if (x - 1 < nx / 2 && x + 1 > nx / 2 && y - 1 < ny / 2 && y + 1 > ny / 2){
-  //     printf("x: %d y: %d z: %d; tu: %f tv: %f tw: %f\\n", x, y, z, tu, tv,
-  //     tw);
+  // if (x - 1 < nx / 2 && x + 1 > nx / 2 && y - 1 < ny / 2 && y + 1 > ny / 2) {
+  //   printf("x: %f \n", output[idx]);
   // }
 }
 
-// Simple transformation kernel
-__global__ void transformKernelRA(float *output, int nx, int ny, int nz,
-                                  float dx, float dy, float dz, float *mat) {
+__global__ void affine3D_RA(float *output, int nx, int ny, int nz, float dx,
+                            float dy, float dz, float *mat) {
 
   // Calculate texture coordinates
   unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -67,5 +117,5 @@ __global__ void transformKernelRA(float *output, int nx, int ny, int nz,
 
   // Read from texture and write to global memory
   int idx = z * (nx * ny) + y * nx + x;
-  output[idx] = tex3D(texRef, tu, tv, tw);
+  output[idx] = tex3D(texref3d, tu, tv, tw);
 }
--- simsim/cuda/transform.py
+++ simsim/cuda/transform.py
@@ -1,75 +1,104 @@
-import pycuda.autoinit
+import pycuda.autoinit  # noqa
 from pycuda.compiler import SourceModule
 import pycuda.driver as cuda
 from pycuda import gpuarray
 import numpy as np
 
-with open(__file__.replace('.py', '.cu'), 'r') as f:
+
+with open(__file__.replace(".py", ".cu"), "r") as f:
     mod_affine = SourceModule(f.read())
 
-_affine = mod_affine.get_function("transformKernel")
-affineRA = mod_affine.get_function("transformKernelRA")
-texref = mod_affine.get_texref("texRef")
-
-
-def with_cupy_conversion(func):
-    def wrapper(*args, **kwargs):
-        args = list(args)
-        array = args[0]
-        if (
-            f"{type(array).__module__}.{type(array).__name__}"
-            == "cupy.core.core.ndarray"
-        ):
-            print("converting from cupy array to pycuda array")
-            array = array.get()
-            pycuda.autoinit.context.push()
-            array = gpuarray.to_gpu(array)
-            args[0] = array
-            v = func(*args, **kwargs)
-            pycuda.autoinit.context.pop()
-            return v
-        return func(*args, **kwargs)
-    return wrapper
+_affine2D = mod_affine.get_function("affine2D")
+_affine2D_RA = mod_affine.get_function("affine2D_RA")
+_affine3D = mod_affine.get_function("affine3D")
+_affine3D_RA = mod_affine.get_function("affine3D_RA")
+texref2D = mod_affine.get_texref("texref2d")
+texref2D.set_address_mode(0, cuda.address_mode.BORDER)
+texref2D.set_address_mode(1, cuda.address_mode.BORDER)
+texref3D = mod_affine.get_texref("texref3d")
+texref3D.set_address_mode(0, cuda.address_mode.BORDER)
+texref3D.set_address_mode(1, cuda.address_mode.BORDER)
+texref3D.set_address_mode(2, cuda.address_mode.BORDER)
 
 
 def _bind_tex(array):
-    pop = False
-    if f"{type(array).__module__}.{type(array).__name__}" == "cupy.core.core.ndarray":
-        print("converting from cupy array to pycuda array")
-        array = array.get()
-        pycuda.autoinit.context.push()
-        pop = True
+    assert array.ndim in (2, 3), "Texture binding only valid for 2 or 3D arrays"
     if isinstance(array, np.ndarray):
         ary = cuda.np_to_array(array, "F" if np.isfortran(array) else "C")
     elif isinstance(array, gpuarray.GPUArray):
         ary = cuda.gpuarray_to_array(array, "F" if array.flags.f_contiguous else "C")
     else:
         raise ValueError("Can only bind numpy arrays or GPUarray")
-    texref.set_array(ary)
-    if pop:
-        pycuda.autoinit.context.pop()
+    if array.ndim == 2:
+        texref2D.set_array(ary)
+    elif array.ndim == 3:
+        texref3D.set_array(ary)
 
 
-def _set_tex_filter_mode(mode):
-    assert mode in ["linear", "point", "nearest"], "unrecognized interpolation mode"
+def _set_tex_filter_mode(mode, ndim):
+    assert mode in [
+        "linear",
+        "point",
+        "nearest",
+    ], f"unrecognized interpolation mode: {mode}"
     if mode == "linear":
         # default is point
-        texref.set_filter_mode(cuda.filter_mode.LINEAR)
+        if ndim == 3:
+            texref3D.set_filter_mode(cuda.filter_mode.LINEAR)
+        elif ndim == 2:
+            texref2D.set_filter_mode(cuda.filter_mode.LINEAR)
+
+
+def _with_bound_texture(func):
+    def wrapper(*args, **kwargs):
+        args = list(args)
+        array = args[0]
+        # so far these all require float32
+        if not array.dtype == np.float32:
+            array = array.astype(np.float32)
+        # bind array to textureRef
+        _bind_tex(array)
+        args[0] = array
+        _set_tex_filter_mode(kwargs.get("mode", "nearest"), ndim=array.ndim)
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def _make_grid(shape, blocks):
+    if len(shape) == 3:
+        out_z, out_y, out_x = shape
+        bx, by, bz = blocks
+        return ((out_x + bx - 1) // bx, (out_y + by - 1) // by, (out_z + bz - 1) // bz)
+    elif len(shape) == 2:
+        out_y, out_x = shape
+        bx, by = blocks[:2]
+        return ((out_x + bx - 1) // bx, (out_y + by - 1) // by, 1)
 
 
 def _make_translation_matrix(mag):
-    tmat = np.eye(4)
-    tmat[0, 3] = mag[2]
-    tmat[1, 3] = mag[1]
-    tmat[2, 3] = mag[0]
+    if len(mag) == 3:
+        tmat = np.eye(4)
+        tmat[0, 3] = mag[2]
+        tmat[1, 3] = mag[1]
+        tmat[2, 3] = mag[0]
+    elif len(mag) == 2:
+        tmat = np.eye(3)
+        tmat[0, 2] = mag[1]
+        tmat[1, 2] = mag[0]
     return tmat
 
 
 def _make_scaling_matrix(scalar):
-    tmat = np.eye(4)
-    tmat[0, 0] = 1 / scalar[2]
-    tmat[1, 1] = 1 / scalar[1]
-    tmat[2, 2] = 1 / scalar[0]
+    if len(scalar) == 3:
+        tmat = np.eye(4)
+        tmat[0, 0] = 1 / scalar[2]
+        tmat[1, 1] = 1 / scalar[1]
+        tmat[2, 2] = 1 / scalar[0]
+    elif len(scalar) == 2:
+        tmat = np.eye(3)
+        tmat[0, 0] = 1 / scalar[1]
+        tmat[1, 1] = 1 / scalar[0]
     return tmat
 
 
@@ -119,147 +148,82 @@ def _make_rotation_matrix(array, angle, axis=0):
     return T
 
 
-@with_cupy_conversion
-def scale(array, scalar=(1, 1, 1), mode="nearest", blocks=(16, 16, 4)):
+def _do_affine(shape, tmat, blocks):
+
+    if len(shape) == 3:
+        if not tmat.shape == (4, 4):
+            raise ValueError(f"3D transformation matrix must be 4x4, saw {tmat.shape}")
+        _func = _affine3D
+        _tref = texref3D
+    elif len(shape) == 2:
+        if not tmat.shape == (3, 3):
+            raise ValueError(f"3D transformation matrix must be 3x3, saw {tmat.shape}")
+        _func = _affine2D
+        _tref = texref2D
+
+    output = gpuarray.empty(shape, dtype=np.float32)
+    _func(
+        output,
+        *np.flip(np.int32(output.shape)),
+        cuda.In(tmat.astype(np.float32).ravel()),
+        texrefs=[_tref],
+        block=blocks,
+        grid=_make_grid(shape, blocks),
+    )
+    return output
+
+
+@_with_bound_texture
+def scale(array, scalar, mode="nearest", blocks=(16, 16, 4)):
     """scale array with nearest neighbors or linear interpolation
 
     scale can be either a scalar or a tuple with len(scale) == array.ndim
     """
     if isinstance(scalar, (int, float)):
-        scalar = (scalar, scalar, scalar)
-
-    _dtype = array.dtype
-    if not _dtype == np.float32:
-        array = array.astype(np.float32)
+        scalar = tuple([scalar] * array.ndim)
+    assert (
+        len(shift) == array.ndim
+    ), "scalar must either be a scalar or a list with the same length as array.ndim"
 
     # make scaling array
     tmat = _make_scaling_matrix(scalar)
 
-    # bind array to textureRef
-    _bind_tex(array)
-    _set_tex_filter_mode(mode)
-
-    out_z = round(array.shape[0] * scalar[0])
-    out_y = round(array.shape[1] * scalar[1])
-    out_x = round(array.shape[2] * scalar[2])
-    output = gpuarray.empty((out_z, out_y, out_x), dtype=np.float32)
-
-    bx, by, bz = blocks
-    grid = ((out_x + bx - 1) // bx, (out_y + by - 1) // by, (out_z + bz - 1) // bz)
-
-    _affine(
-        cuda.Out(output),
-        np.int32(out_x),
-        np.int32(out_y),
-        np.int32(out_z),
-        cuda.In(tmat.astype(np.float32).ravel()),
-        texrefs=[texref],
-        block=blocks,
-        grid=grid,
-    )
-    return output
+    outshape = tuple(np.round(np.array(array.shape) * scalar).astype(np.int))
+    return _do_affine(outshape, tmat, blocks)
 
 
-@with_cupy_conversion
+@_with_bound_texture
 def rotate(array, angle, axis=0, mode="nearest", blocks=(16, 16, 4)):
     """rotate array around a single axis
 
     axis can be either 0,1,2 or z,y,x
     """
-    _dtype = array.dtype
-    if not _dtype == np.float32:
-        array = array.astype(np.float32)
-
-    # make scaling array
     tmat = _make_rotation_matrix(array, angle, axis)
-    # bind array to textureRef
-    _bind_tex(array)
-    _set_tex_filter_mode(mode)
-
-    out_z, out_y, out_x = array.shape
-    output = gpuarray.empty((out_z, out_y, out_x), dtype=np.float32)
-    bx, by, bz = blocks
-    grid = ((out_x + bx - 1) // bx, (out_y + by - 1) // by, (out_z + bz - 1) // bz)
-
-    _affine(
-        output,
-        np.int32(out_x),
-        np.int32(out_y),
-        np.int32(out_z),
-        cuda.In(tmat.astype(np.float32).ravel()),
-        texrefs=[texref],
-        block=blocks,
-        grid=grid,
-    )
-    return output
+    return _do_affine(array.shape, tmat, blocks)
 
 
-@with_cupy_conversion
-def translate(array, mag=(0, 0, 0), mode="nearest", blocks=(16, 16, 4)):
+@_with_bound_texture
+def shift(array, shift, mode="nearest", blocks=(16, 16, 4)):
     """translate array
 
     mag is number of pixels to translate in (z,y,x)
     must be tuple with length array.ndim
     """
-    _dtype = array.dtype
-    if not _dtype == np.float32:
-        array = array.astype(np.float32)
+    if isinstance(shift, (int, float)):
+        shift = tuple([shift] * array.ndim)
+    assert (
+        len(shift) == array.ndim
+    ), "shift must either be a scalar or a list with the same length as array.ndim"
 
-    # make scaling array
-    tmat = _make_translation_matrix(mag)
-
-    # bind array to textureRef
-    _bind_tex(array)
-    _set_tex_filter_mode(mode)
-
-    out_z, out_y, out_x = array.shape
-    output = gpuarray.empty((out_z, out_y, out_x), dtype=np.float32)
-    bx, by, bz = blocks
-    grid = ((out_x + bx - 1) // bx, (out_y + by - 1) // by, (out_z + bz - 1) // bz)
-
-    _affine(
-        output,
-        np.int32(out_x),
-        np.int32(out_y),
-        np.int32(out_z),
-        cuda.In(tmat.astype(np.float32).ravel()),
-        texrefs=[texref],
-        block=blocks,
-        grid=grid,
-    )
-    return output
+    tmat = _make_translation_matrix(shift)
+    return _do_affine(array.shape, tmat, blocks)
 
 
-@with_cupy_conversion
+@_with_bound_texture
 def affine(array, tmat, mode="nearest", blocks=(16, 16, 4)):
     """translate array
 
     mag is number of pixels to translate in (z,y,x)
     must be tuple with length array.ndim
     """
-    _dtype = array.dtype
-    if not _dtype == np.float32:
-        array = array.astype(np.float32)
-
-    assert tmat.shape == (4, 4), "transformation matrix must have shape (4, 4)"
-
-    # bind array to textureRef
-    _bind_tex(array)
-    _set_tex_filter_mode(mode)
-
-    out_z, out_y, out_x = array.shape
-    output = gpuarray.empty((out_z, out_y, out_x), dtype=np.float32)
-    bx, by, bz = blocks
-    grid = ((out_x + bx - 1) // bx, (out_y + by - 1) // by, (out_z + bz - 1) // bz)
-
-    _affine(
-        output,
-        np.int32(out_x),
-        np.int32(out_y),
-        np.int32(out_z),
-        cuda.In(tmat.astype(np.float32).ravel()),
-        texrefs=[texref],
-        block=blocks,
-        grid=grid,
-    )
-    return output
+    return _do_affine(array.shape, tmat, blocks)
